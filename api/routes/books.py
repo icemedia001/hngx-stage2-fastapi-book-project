c5c7b22d2d307c1b39959ff717e7f1e754818823
Cold Dev <96265196+icemedia001@users.noreply.github.com>
2025-02-12 13:02:15 -0800

    feat: Update books.py

--- api/routes/books.py
+++ api/routes/books.py
@@ -62,12 +62,12 @@ async def delete_book(book_id: int) -> None:
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
 
 
-# @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
-# async def get_book(book_id: int) -> Book:
-#     book = db.get_book(book_id)
-#     if book is None:
-#         raise HTTPException(
-#             status_code=status.HTTP_404_NOT_FOUND,
-#             detail=f"Book with id {book_id} not found"
-#         )
-#     return book
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    book = db.get_book(book_id)
+    if book is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Book with id {book_id} not found"
+        )
+    return book
